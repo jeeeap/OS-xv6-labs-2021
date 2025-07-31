6fd23ba106b982a15b597460a60c8af0fc49b136
jeeeap <jp3274313334@163.com>
2025-08-01 03:42:18 +0800

    2025.8.1--lab2-trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -136,7 +136,7 @@ static char *syscall_names[] = {
 "read", "kill", "exec", "fstat", "chdir",  
 "dup", "getpid", "sbrk", "sleep", "uptime",  
 "open", "write", "mknod", "unlink", "link",  
-"mkdir", "close", "trace", "sysinfo"}; 
+"mkdir", "close", "trace"}; 
 
 void
 syscall(void)
@@ -148,12 +148,13 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
     if((1<<num) & p->mask) {
-    printf("%d: syscall %s -> %d\n", 
-           p->pid, syscall_names[num], p->trapframe->a0);
-  } else {
+      printf("%d: syscall %s -> %d\n", 
+              p->pid, syscall_names[num], p->trapframe->a0);
+    }
+  } 
+  else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
-    }
   }
 }
